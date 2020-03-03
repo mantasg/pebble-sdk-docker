36ec8c8ad913a4132b11ef8271105a6a162c65f0
Mantas Gudynas <mantas.gudynas@gmail.com>
2020-03-03 20:38:32 +0000

    Resolving full path for mounted working dir

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 WORKING_DIR="$1"
 [ -z "$WORKING_DIR" ] && echo "Please specify local working directory" && exit 1
+WORKING_DIR=$(realpath "$WORKING_DIR")
 
 docker run -it --net=host --env="DISPLAY" \
   --volume="$HOME/.Xauthority:/root/.Xauthority" \
