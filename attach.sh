98164873bddecb2027d25a7af5c74df067ab43b6
Mantas Gudynas <mantas.gudynas@tracsis.com>
2020-08-09 21:13:56 +0100

    update

--- attach.sh
+++ attach.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker exec -it pebble-sdk bash
+CONTAINER_NAME=$(docker ps -a | grep pebble-sdk | awk 'NF>1{print $NF}' | head -n 1)
+[ -z "$CONTAINER_NAME" ] && { echo "No pebble-sdk container running"; exit; }
+
+docker exec -it "$CONTAINER_NAME" bash
--- run.sh
+++ run.sh
@@ -4,10 +4,13 @@ WORKING_DIR="$1"
 [ -z "$WORKING_DIR" ] && echo "Please specify local working directory" && exit 1
 WORKING_DIR=$(realpath "$WORKING_DIR")
 
+touch "$HOME/.cache/pebble-sdk-bash-history"
+
 docker run -it --net=host --env="DISPLAY" \
   --volume="$HOME/.Xauthority:/root/.Xauthority" \
   --volume="$WORKING_DIR:/work:rw" \
+  --volume="$WORKING_DIR:/work:rw" \
+  --volume="$HOME/.cache/pebble-sdk-bash-history:/root/.bash_history:rw" \
   --workdir="/work" \
-  --name="pebble-sdk" \
   pebble-sdk \
   bash
